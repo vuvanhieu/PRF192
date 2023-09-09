8d6d884b59e4510fc0255821395bec574f3adf0b
vuvanhieu <vuvanhieucntt@gmail.com>
2023-09-09 11:33:40 +0700

    Create example_4.c

---
+++ Slot_1/example_4.c
@@ -0,0 +1,11 @@
+//Hay nhap x tu ban phim va hien thi
+#include<stdio.h>
+int main(){
+	int x;
+	printf("Moi ngai nhap so x tu ban phim:"); 
+	scanf("%d",&x);
+//	scanf("%d%d",&x1,&x2)
+	printf("So ngai vua nhap la: %d", x);
+	
+	return 0;
+}
