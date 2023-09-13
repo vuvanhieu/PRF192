92debc238a73aa34fb6c8959a7bffd95b1542b95
vuvanhieu <vuvanhieucntt@gmail.com>
2023-09-13 16:21:31 +0700

    Create ex1.c

---
+++ Slot_3/ex1.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    system("cls"); // 
+
+    int a, b, c;
+    printf("Nhap a, b, và c:\n");
+    scanf("%d%d%d", &a, &b, &c);
+
+    float delta = (float)(b * b - 4 * a * c);
+
+    if (delta < 0) {
+        printf("Phuong trinh vo nghiem.\n");
+    } else if (delta == 0) {
+        printf("Phuong trinh co nghiem kep.\n");
+        printf("x1 = x2 = %f\n", -b / (2.0 * a));
+    } else {
+        printf("Phuong trinh co hai nghiem:\n");
+        float x1 = (-b - sqrt(delta)) / (2.0 * a);
+        float x2 = (-b + sqrt(delta)) / (2.0 * a);
+        printf("x1 = %f\n", x1);
+        printf("x2 = %f\n", x2);
+    }
+
+    return 0;
+}
+
